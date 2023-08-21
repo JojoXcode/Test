2d2053887a3b54fc0f78b3adcd208cad2a2a2a7b
Johannes <test@wolfysoft.de>
2023-08-21 14:46:50 +0200

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Jojo on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
